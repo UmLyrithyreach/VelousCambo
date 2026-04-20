7fe5861d2b3db9208ec9e99640237b51cef35d7e
UmLyrithyreach <umlyrithyreach@gmail.com>
2026-04-20 16:11:28 +0700

    feat: wire up app entry point with Firebase init, providers and route definitions

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,92 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:velouscambo_enhanced_new/app.dart';
+import 'package:velouscambo_enhanced_new/firebase_options.dart';
+import 'package:velouscambo_enhanced_new/features/auth/viewmodel/auth_viewmodel.dart';
+import 'package:velouscambo_enhanced_new/features/history/viewmodel/history_viewmodel.dart';
+import 'package:velouscambo_enhanced_new/features/map/viewmodel/station_viewmodel.dart';
+import 'package:velouscambo_enhanced_new/features/profile/viewmodel/profile_viewmodel.dart';
+import 'package:velouscambo_enhanced_new/features/ride/viewmodel/ride_viewmodel.dart';
+import 'package:velouscambo_enhanced_new/features/search/viewmodel/search_viewmodel.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.linux) {
+    runApp(const _UnsupportedPlatformApp());
+    return;
+  }
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    runApp(_InitErrorApp(error: e.toString()));
+    return;
+  }
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthViewModel()),
+        ChangeNotifierProvider(create: (_) => StationViewModel()),
+        ChangeNotifierProvider(create: (_) => HistoryViewModel()),
+        ChangeNotifierProvider(create: (_) => RideViewModel()),
+        ChangeNotifierProvider(create: (_) => ProfileViewModel()),
+        ChangeNotifierProvider(create: (_) => SearchViewModel()),
+      ],
+      child: const VelousCamboApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _InitErrorApp extends StatelessWidget {
+  final String error;
+  const _InitErrorApp({required this.error});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Text(
+              'Firebase init failed:\n$error\n\nRun with:\nflutter run -d android --dart-define-from-file=.env',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _UnsupportedPlatformApp extends StatelessWidget {
+  const _UnsupportedPlatformApp();
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Linux desktop is not supported.\nRun on Android or Web:\nflutter run -d android --dart-define-from-file=.env',
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
