8b085ac9ddd95d47cdb9edbd8a0d657ae0b46afe
XRockKimu <kimkheng.ly@student.cadt.edu.kh>
2026-04-22 01:39:13 +0700

    feat: integrate booking, QR scan, and payment navigation flow

--- lib/app.dart
+++ lib/app.dart
@@ -6,6 +6,9 @@ import 'package:velouscambo_enhanced_new/features/auth/view/register_screen.dart
 import 'package:velouscambo_enhanced_new/features/main/view/main_screen.dart';
 import 'package:velouscambo_enhanced_new/features/ride/view/booking_screen.dart';
 import 'package:velouscambo_enhanced_new/features/ride/view/active_rental_screen.dart';
+import 'package:velouscambo_enhanced_new/features/ride/view/qr_scanner_screen.dart';
+import 'package:velouscambo_enhanced_new/features/ride/view/payment_type_screen.dart';
+import 'package:velouscambo_enhanced_new/features/ride/view/booking_confirmed_screen.dart';
 import 'package:velouscambo_enhanced_new/features/profile/view/edit_profile_screen.dart';
 import 'package:velouscambo_enhanced_new/features/profile/view/subscription_screen.dart';
 import 'package:velouscambo_enhanced_new/features/search/view/search_screen.dart';
@@ -20,16 +23,37 @@ class VelousCamboApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       initialRoute: '/',
-      routes: {
-        '/': (_) => const SplashScreen(),
-        '/login': (_) => const LoginScreen(),
-        '/register': (_) => const RegisterScreen(),
-        '/home': (_) => const MainScreen(),
-        '/search': (_) => const SearchScreen(),
-        '/booking': (_) => const BookingScreen(),
-        '/active-rental': (_) => const ActiveRentalScreen(),
-        '/edit-profile': (_) => const EditProfileScreen(),
-        '/subscription': (_) => const SubscriptionScreen(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(builder: (_) => const SplashScreen());
+          case '/login':
+            return MaterialPageRoute(builder: (_) => const LoginScreen());
+          case '/register':
+            return MaterialPageRoute(builder: (_) => const RegisterScreen());
+          case '/home':
+            return MaterialPageRoute(builder: (_) => const MainScreen());
+          case '/search':
+            return MaterialPageRoute(builder: (_) => const SearchScreen());
+          case '/booking':
+            return MaterialPageRoute(builder: (_) => const BookingScreen(), settings: settings);
+          case '/booking-confirmed':
+            final bikeId = settings.arguments as String;
+            return MaterialPageRoute(builder: (_) => BookingConfirmedScreen(bikeId: bikeId));
+          case '/qr-scanner':
+            final bikeId = settings.arguments as String;
+            return MaterialPageRoute(builder: (_) => QRScannerScreen(expectedBikeId: bikeId));
+          case '/payment-type':
+            return MaterialPageRoute(builder: (_) => const PaymentTypeScreen(), settings: settings);
+          case '/active-rental':
+            return MaterialPageRoute(builder: (_) => const ActiveRentalScreen());
+          case '/edit-profile':
+            return MaterialPageRoute(builder: (_) => const EditProfileScreen());
+          case '/subscription':
+            return MaterialPageRoute(builder: (_) => const SubscriptionScreen(), settings: settings);
+          default:
+            return MaterialPageRoute(builder: (_) => const SplashScreen());
+        }
       },
     );
   }
